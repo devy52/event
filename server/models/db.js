4e2569bcc770bf7e9d5df9e75d2d03194d40fac3
devy52 <108571763+devy52@users.noreply.github.com>
2023-09-08 11:08:43 +0530

    Update1 db.js

--- server/models/db.js
+++ server/models/db.js
@@ -1,7 +1,7 @@
 // db.js
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://dev52:root@cluster0.msyyjkw.mongodb.net/gdsc', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://dev52:<password>@cluster0.msyyjkw.mongodb.net/gdsc', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Connected to MongoDB');
   })
